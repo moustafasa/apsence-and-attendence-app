cd85101cb20b87b6e3e1ce63f2afb9e06e396d26
moustafa saad <moustafasaad954@gmail.com>
2024-07-20 12:51:46 +0300

    add footer to attendence table

--- src/app/components/AttendenceTable.tsx
+++ src/app/components/AttendenceTable.tsx
@@ -1,3 +1,4 @@
+import cn from "@/lib/cssConditional";
 import formatTime from "@/lib/formatTime";
 import getDayDate from "@/lib/getDayDate";
 import Link from "next/link";
@@ -10,15 +11,23 @@ export default function AttendenceTable({ attendence }: Props) {
     const att = attendence.find((att) => att.id === index + 1);
     const endDate = att && att.endDate && formatTime(new Date(att.endDate));
     return (
-      <tr key={att?.id || index + 1} className="odd:bg-black-400">
-        <th className="py-5 ">{index + 1}</th>
-        <td className="py-5">{att && formatTime(new Date(att.startDate))}</td>
-        <td className="py-5">{endDate}</td>
-        <td className="py-5">
+      <tr
+        key={att?.id || index + 1}
+        className={cn("odd:bg-black-400", {
+          "outline-2 outline-green-100 outline-solid rounded-lg":
+            todayDate.getDate() === index + 1,
+        })}
+      >
+        <th className="px-3 py-5 ">{index + 1}</th>
+        <td className="px-3 py-5">
+          {att && formatTime(new Date(att.startDate))}
+        </td>
+        <td className="px-3 py-5">{endDate}</td>
+        <td className="px-3 py-5">
           {att && (att.numberOfHours / 1000 / 60 / 60).toFixed(2) + " hours"}
         </td>
-        <td className="py-5">
-          {index + 1 <= todayDate.getDate() && (
+        <td className="px-3 py-5">
+          {index + 1 < todayDate.getDate() && (
             <Link
               href={`/dashboard/edit/${att?.id || index + 1}`}
               className="capitalize bg-green-100 transition-colors duration-300 hover:bg-green-200 shadow-sm px-4 py-1 rounded-lg"
@@ -32,27 +41,35 @@ export default function AttendenceTable({ attendence }: Props) {
   });
 
   return (
-    <div className="p-5 overflow-x-auto">
-      <table className="min-w-full border-separate capitalize table-fixed border-spacing-0 text-center shadow-lg">
+    <div className="p-5 overflow-auto ">
+      <table className="min-w-full border-separate capitalize table-fixed border-spacing-0 text-center shadow-lg text-nowrap">
         <thead className="">
           <tr className="bg-black-100">
-            <th className=" rounded-ss-lg py-5 w-[70px]">days</th>
-            <th className="py-5">start</th>
-            <th className=" py-5">end</th>
-            <th className=" py-5">total hours</th>
-            <th className="rounded-se-lg py-5">options</th>
+            <th className=" rounded-ss-lg px-3 py-5 w-[70px]">days</th>
+            <th className="px-3 py-5 w-[200px]">start</th>
+            <th className=" px-3 py-5 w-[200px]">end</th>
+            <th className=" px-3 py-5 w-[200px]">total hours</th>
+            <th className="rounded-se-lg px-3 py-5 w-[200px]">options</th>
           </tr>
         </thead>
         <tbody>{tableData}</tbody>
+        <tfoot className="">
+          <tr className="bg-black-200">
+            <th className="px-3 py-5 text-2xl rounded-es-lg" colSpan={2}>
+              total month hours :
+            </th>
+            <td
+              colSpan={3}
+              className=" text-blue-400 font-bold text-3xl py-5 px-3 text-center rounded-ee-lg"
+            >
+              300 hours
+            </td>
+          </tr>
+        </tfoot>
       </table>
-      <div className="mt-6 p-10">
-        <h3 className="capitalize text-2xl flex items-center gap-3 text-blue-400 ">
-          total month hours: <span className="">{300} hours</span>
-        </h3>
-        <button className="mx-auto block w-fit mt-5 capitalize bg-green-100 p-3 rounded-lg shadow-lg hover:bg-green-200 transition-colors duration-500">
-          calc salary
-        </button>
-      </div>
+      <button className="mx-auto block w-fit mt-5 capitalize bg-green-100 p-3 rounded-lg shadow-lg hover:bg-green-200 transition-colors duration-500 mb-5">
+        calc salary
+      </button>
     </div>
   );
 }
