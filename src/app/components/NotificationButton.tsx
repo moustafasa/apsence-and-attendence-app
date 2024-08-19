daf3dec40ab3c3d3207e5f8c750bd692da206409
moustafa saad <moustafasaad954@gmail.com>
2024-08-20 01:50:01 +0300

    fdsa

--- src/app/components/NotificationButton.tsx
+++ src/app/components/NotificationButton.tsx
@@ -36,21 +36,27 @@ export default function NotificationButton() {
   }, []);
 
   return (
-    <div className="relative flex items-center">
+    <div className=" flex items-center relative h-full ">
       <button className="text-xl relative">
         <sub className="grid place-content-center text-xs bg-red-600 absolute aspect-square h-5  left-0 top-0 -translate-x-1/2 -translate-y-1/2  rounded-full ">
           {notifications.filter((notify) => !notify.read).length}
         </sub>
         <IoNotifications />
       </button>
-      <ul className="absolute hidden ">
-        {notifications.map((notify) => (
-          <li key={notify.id} className="">
-            {notify.type === "salaryRequest" &&
-              `${notify.from} ask you to paid his salary`}
-          </li>
-        ))}
-      </ul>
+      <div className="absolute min-w-max p-3 right-0 top-full z-50 translate-y-3 bg-black-100  rounded-lg before:absolute before:w-0 before:h-0 before:border-[10px] before:top-0 before:right-0 before:-translate-y-[calc(100%-5px)] before:border-transparent before:border-b-black-100  backdrop:blur-lg ">
+        <h3 className="text-3xl mb-3 mt-3">notifications</h3>
+        <ul>
+          {notifications.map((notify) => (
+            <li
+              key={notify.id}
+              className="p-3 hover:bg-blue-200 rounded-lg transition-colors duration-300 cursor-pointer"
+            >
+              {notify.type === "salaryRequest" &&
+                `${notify.from} ask you to paid his salary`}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
