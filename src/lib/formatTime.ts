c59edd1c9975ab0192c61b789a5ddc2bb8dd7db5
moustafa ramadan moustafa saad 6191176 <moustafasaad954@gmail.com>
2025-03-05 05:29:48 +0200

    jsf

--- src/lib/formatTime.ts
+++ src/lib/formatTime.ts
@@ -1,6 +1,6 @@
 import { format } from "date-fns";
 
 export default function formatTime(time: Date, hour12: boolean = true) {
-  console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+  console.log(Intl.DateTimeFormat().resolvedOptions().timeZoneName);
   return format(time.toString(), `hh:mm${hour12 ? " a" : ""}`);
 }
