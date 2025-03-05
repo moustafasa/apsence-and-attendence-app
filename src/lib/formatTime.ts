857c6323a98c4584e3c4ab27552027a3511a9f05
moustafa ramadan moustafa saad 6191176 <moustafasaad954@gmail.com>
2025-03-05 04:19:31 +0200

    fdskla

--- src/lib/formatTime.ts
+++ src/lib/formatTime.ts
@@ -2,5 +2,8 @@ import { TZDate } from "@date-fns/tz";
 import { format } from "date-fns";
 
 export default function formatTime(time: Date, hour12: boolean = true) {
-  return format(new TZDate(time), `hh:mm${hour12 ? " a" : ""}`);
+  return format(
+    new TZDate(time, Intl.DateTimeFormat().resolvedOptions().timeZone),
+    `hh:mm${hour12 ? " a" : ""}`
+  );
 }
